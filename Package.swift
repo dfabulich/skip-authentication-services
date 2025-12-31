4d4df502462908bcca50d2fefecb274b8b35b177
Dan Fabulich <dan@fabulich.com>
2025-12-30 16:16:44 -0800

    WebAuthenticationSession

--- Package.swift
+++ Package.swift
@@ -11,11 +11,11 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://source.skip.tools/skip.git", from: "1.6.30"),
-        .package(url: "https://source.skip.tools/skip-foundation.git", from: "1.0.0")
+        .package(url: "https://source.skip.tools/skip-ui.git", from: "1.0.0")
     ],
     targets: [
         .target(name: "SkipAuthenticationServices", dependencies: [
-            .product(name: "SkipFoundation", package: "skip-foundation")
+            .product(name: "SkipUI", package: "skip-ui")
         ], resources: [.process("Resources")], plugins: [.plugin(name: "skipstone", package: "skip")]),
         .testTarget(name: "SkipAuthenticationServicesTests", dependencies: [
             "SkipAuthenticationServices",
@@ -23,3 +23,15 @@ let package = Package(
         ], resources: [.process("Resources")], plugins: [.plugin(name: "skipstone", package: "skip")]),
     ]
 )
+
+if Context.environment["SKIP_BRIDGE"] ?? "0" != "0" {
+    package.dependencies += [.package(url: "https://source.skip.tools/skip-fuse-ui.git", from: "1.0.0")]
+    package.targets.forEach({ target in
+        target.dependencies += [.product(name: "SkipFuseUI", package: "skip-fuse-ui")]
+    })
+    // all library types must be dynamic to support bridging
+    package.products = package.products.map({ product in
+        guard let libraryProduct = product as? Product.Library else { return product }
+        return .library(name: libraryProduct.name, type: .dynamic, targets: libraryProduct.targets)
+    })
+}
